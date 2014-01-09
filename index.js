247a335f261a66f8b7c3df0393e4c51691c14e4d
streetlight <nweingartner@gmail.com>
2014-01-09 18:57:35 -0500

    Structure change

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /* Express 3 requires that you instantiate a `http.Server` to attach socket.io to first */
 var app = require('express')(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    port = 8080,
-    url  = 'http://localhost:' + port + '/';
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  port = 8080,
+  db = require('./lib/couchdb'),
+  url = 'http://localhost:' + port + '/';
 
 server.listen(port);
 console.log("Express server listening on port " + port);
@@ -17,7 +18,9 @@ app.get('/', function (req, res) {
 io.sockets.on('connection', function (socket) {
 
   // Emit a message to send it to the client.
-  socket.emit('ping', { msg: 'Hello. I know socket.io.' });
+  socket.emit('ping', {
+    msg: 'Hello. I know socket.io.'
+  });
 
   // Print messages from the client.
   socket.on('pong', function (data) {
