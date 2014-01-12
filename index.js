d41ccd3c7f9fc7bfbbe054a9b78d7a69dd5bce06
streetlight <nweingartner@gmail.com>
2014-01-12 15:49:44 -0500

    Get sockets working cross domain

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 /* Express 3 requires that you instantiate a `http.Server` to attach socket.io to first */
 var app = require('express')(),
-  server = require('http').createServer(app),
-  io = require('socket.io').listen(server),
-  port = 8080,
-  db = require('./lib/couchdb'),
-  url = 'http://localhost:' + port + '/';
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+	port = 8080,
+	db = require('./lib/couchdb'),
+	url = 'http://localhost:' + port + '/';
 
 server.listen(port);
 console.log("Express server listening on port " + port);
 console.log(url);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+	res.sendfile(__dirname + '/index.html');
 });
 
 //Socket.io emits this event when a connection is made.
 io.sockets.on('connection', function (socket) {
 
-  // Emit a message to send it to the client.
-  socket.emit('ping', {
-    msg: 'Hello. I know socket.io.'
-  });
+	db.get('testing', function (err, doc) {
+		console.log(doc);
+		// Emit a message to send it to the client.
+		socket.emit('testing', doc);
+	});
 
-  // Print messages from the client.
-  socket.on('pong', function (data) {
-    console.log(data.msg);
-  });
+	/*// Emit a message to send it to the client.
+	socket.emit('ping', {
+		msg: 'Hello. I know socket.io.'
+	});
+
+	// Print messages from the client.
+	socket.on('pong', function (data) {
+		console.log(data.msg);
+	}); */
 
 });
